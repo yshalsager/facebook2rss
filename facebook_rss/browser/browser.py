ee3f6ca17356f160ceb60d9fb13eb4980456394d
yshalsager <ysh-alsager@hotmail.com>
2021-02-07 21:26:17 +0200

    browser: use proxy configuration from settings object
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- facebook_rss/browser/browser.py
+++ facebook_rss/browser/browser.py
@@ -4,27 +4,33 @@ from typing import Optional
 from playwright.async_api import async_playwright, Browser as PBrowser, Playwright, BrowserContext
 
 from facebook_rss import local_cookies
-from facebook_rss.utils.decorators import singleton
+from facebook_rss.config import get_settings, Settings
 from facebook_rss.utils.pickling import pickle_, unpickle
 
 logger = logging.getLogger(__name__)
 
 
-@singleton
 class Browser:
 
-    def __init__(self):
+    def __init__(self, settings):
         self._playwright: Optional[Playwright] = None
         self._browser: Optional[PBrowser] = None
         self._browser_context: Optional[BrowserContext] = None
+        self._settings: Settings = settings
 
     async def start(self, *args, **kwargs):
         if not self._playwright:
             self._playwright = await async_playwright().start()
             logger.info("Playwright started")
         if not self._browser:
-            self._browser = await self._playwright.chromium.launch(**kwargs)
-            logger.info("Browser launched")
+            if self._settings.PROXY_SERVER and self._settings.PROXY_USERNAME and self._settings.PROXY_PASSWORD:
+                self._browser = await self._playwright.chromium.launch(
+                    proxy={"server": self._settings.PROXY_SERVER, "username": self._settings.PROXY_USERNAME,
+                           "password": self._settings.PROXY_PASSWORD}, **kwargs)
+                logger.info(f"Browser launched with proxy {self._settings.PROXY_SERVER}")
+            else:
+                self._browser = await self._playwright.chromium.launch(**kwargs)
+                logger.info("Browser launched")
         if not self._browser_context:
             self._browser_context = await self._browser.new_context(locale='en_US')
             logger.info("New context created")
@@ -50,7 +56,7 @@ class Browser:
 
 # Dependency
 async def get_browser() -> Browser:
-    browser = Browser()
+    browser = Browser(get_settings())
     await browser.start(headless=True)
     await browser.add_cookies(unpickle(local_cookies))
     try:
