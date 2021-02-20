34de2da23c75d0c666140372117fa28928d25a50
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2021-02-20 20:19:47 +0200

    docker: improvements
    * load environment from .env file
    * disable login step in entry point script and it's required only one time
    * remove session file from dockerignore to allow copying it to the docker image
    
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 -m  facebook_rss --login -u $EMAIL -p $PASSWORD
+#python3 -m facebook_rss --login -u $EMAIL -p $PASSWORD
 uvicorn facebook_rss.main:api --host 0.0.0.0 --port 8000
