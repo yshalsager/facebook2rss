956799ad8e9ec19457005829f1c696325610158e
yshalsager <ysh-alsager@hotmail.com>
2021-02-23 18:54:41 +0200

    browser: switch to persistent context
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- facebook_rss/browser/browser.py
+++ facebook_rss/browser/browser.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Optional
 
-from playwright.async_api import async_playwright, Browser as PBrowser, Playwright, BrowserContext
+from playwright.async_api import async_playwright, Playwright, BrowserContext
 
 from facebook_rss import local_cookies
 from facebook_rss.config import get_settings, Settings
@@ -14,39 +14,42 @@ class Browser:
 
     def __init__(self, settings=None):
         self._playwright: Optional[Playwright] = None
-        self._browser: Optional[PBrowser] = None
-        self._browser_context: Optional[BrowserContext] = None
+        self._browser: Optional[BrowserContext] = None
         self._settings: Settings = settings
 
     async def start(self, *args, **kwargs):
         if not self._playwright:
             self._playwright = await async_playwright().start()
         if not self._browser:
-            if self._settings and self._settings.PROXY_SERVER and self._settings.PROXY_USERNAME and self._settings.PROXY_PASSWORD:
-                self._browser = await self._playwright.chromium.launch(
-                    proxy={"server": self._settings.PROXY_SERVER, "username": self._settings.PROXY_USERNAME,
-                           "password": self._settings.PROXY_PASSWORD}, **kwargs)
+            if self._settings and self._settings.PROXY_SERVER \
+                    and self._settings.PROXY_USERNAME and self._settings.PROXY_PASSWORD:
+                self._browser = await self._playwright.chromium.launch_persistent_context(
+                    "browser_data/", java_script_enabled=True, args=["--disable-notifications"],
+                    proxy={
+                        "server": self._settings.PROXY_SERVER,
+                        "username": self._settings.PROXY_USERNAME,
+                        "password": self._settings.PROXY_PASSWORD},
+                    **kwargs)
                 logger.info(f"Browser launched with proxy {self._settings.PROXY_SERVER}")
             else:
-                self._browser = await self._playwright.chromium.launch(**kwargs)
-        if not self._browser_context:
-            self._browser_context = await self._browser.new_context(locale='en_US')
+                self._browser = await self._playwright.chromium.launch_persistent_context(
+                    "browser_data/", java_script_enabled=True, args=["--disable-notifications"], **kwargs)
 
     async def new_page(self):
-        return await self._browser_context.new_page()
+        return await self._browser.new_page()
 
     async def add_cookies(self, cookies):
         if cookies:
-            await self._browser_context.add_cookies(cookies)
+            await self._browser.add_cookies(cookies)
 
     async def shutdown(self):
-        await self._browser_context.close()
+        # await self._browser_context.close()
         await self._browser.close()
         await self._playwright.stop()
 
     @property
     async def cookies(self):
-        return await self._browser_context.cookies()
+        return await self._browser.cookies()
 
 
 # Dependency
